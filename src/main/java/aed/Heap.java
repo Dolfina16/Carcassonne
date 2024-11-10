5a919d92d57d8d58a27dbd86dd240567dbfb1a52
unknown <ramiro.ciruzzi@gmail.com>
2024-11-10 18:11:28 -0300

    fix compare >0

--- src/main/java/aed/Heap.java
+++ src/main/java/aed/Heap.java
@@ -21,7 +21,7 @@ public Heap(T[] arreglo, Comparator<T> comparador){
 public void heapify(int pos){
     if(tamaño > 2 && pos>1){
         if(elementos.get((pos-1)/2) == elementos.get((pos-2)/2)){
-            siftDown(new int[]{(pos-1)/2,pos,pos-1});
+            siftDown(new int[]{(pos-1)/2,pos-1,pos});
             heapify(pos-=2);
         }else{
             siftDown(new int[]{(pos-1)/2,pos});
@@ -35,13 +35,13 @@ public void heapify(int pos){
 public void siftDown(int[] elems){
     T padre = elementos.get(elems[0]);
     if(elems.length == 2){
-            if(comparador.compare(padre, elementos.get(elems[1])) < 1){
+            if(comparador.compare(padre, elementos.get(elems[1])) < 0){
                 elementos.set(elems[0], elementos.get(elems[1]));
                 elementos.set(elems[1], padre);
             }
     }else if(elems.length == 3){
-        int hijoMayor = comparador.compare(elementos.get(elems[1]), elementos.get(elems[2])) > 1 ? elems[1] : elems[2];
-        if(comparador.compare(padre, elementos.get(hijoMayor)) < 1){
+        int hijoMayor = comparador.compare(elementos.get(elems[1]), elementos.get(elems[2])) > 0 ? elems[1] : elems[2];
+        if(comparador.compare(padre, elementos.get(hijoMayor)) < 0){
             elementos.set(elems[0], elementos.get(hijoMayor));
             elementos.set(hijoMayor, padre);
         }
@@ -51,13 +51,13 @@ public void siftDown(int[] elems){
 
 public static void main(String[] args) {
     Comparator<Ciudad> SuperavitComparator = Comparator.comparing(Ciudad::superavit);
-    Ciudad ciudad1 = new Ciudad(0);
-    Ciudad ciudad = new Ciudad(1);
+    Ciudad ciudad0 = new Ciudad(0);
+    Ciudad ciudad1 = new Ciudad(1);
     Ciudad ciudad2 = new Ciudad(2);
-    ciudad.incr_ganancia(200);
-    ciudad1.incr_ganancia(10);
-    ciudad2.incr_perdida(400);
-    Heap<Ciudad> heap = new Heap<Ciudad>(new Ciudad[]{ciudad2,ciudad1,ciudad}, SuperavitComparator);
+    ciudad0.incr_ganancia(200); //200 super
+    ciudad1.incr_ganancia(10); //10 super
+    ciudad2.incr_perdida(400); //-400 super
+    Heap<Ciudad> heap = new Heap<Ciudad>(new Ciudad[]{ciudad2,ciudad1,ciudad0}, SuperavitComparator);
 }
 
 }
