383c358d583274bc00c05a447dc8ab420301629e
unknown <ramiro.ciruzzi@gmail.com>
2024-11-10 21:37:50 -0300

    gotica

--- src/main/java/aed/Heap.java
+++ src/main/java/aed/Heap.java
@@ -18,6 +18,77 @@ public Heap(T[] arreglo, Comparator<T> comparador){
     heapify(tamaño-1);
 }
 
+public static int log2(int N){
+    int result = (int)(Math.log(N) / Math.log(2));
+
+    return result;
+}
+
+public T maximus(){
+    return elementos.getFirst();
+}
+
+public T sacarMaximo(){
+    T hashirama;
+    if (tamaño == 0) {
+        return null;
+    }else if (tamaño < 3) {
+        hashirama = elementos.removeFirst();
+        tamaño--;
+        return hashirama;
+    }else{
+        tamaño--;
+        hashirama = elementos.getFirst();
+        elementos.set(0, elementos.removeLast());
+        int pos = 0;
+        while (pos <= (Math.pow(2, Math.floor(log2(tamaño))) - 2)) {
+            if ((2*pos)+2 < tamaño) {
+                T elemMovedizo = elementos.get(pos);
+                siftDown(new int[]{pos,(2*pos)+1,(2*pos)+2});
+                if (elementos.get(pos).equals(elemMovedizo)) {
+                    //no me movi
+                    pos = tamaño;
+                }else if (elementos.get((2*pos)+1).equals(elemMovedizo)) {
+                    //me fui al hijo izq
+                    pos = (2*pos)+1;
+                }else{
+                    //me fui al hijo der
+                    pos = (2*pos)+2;
+                }
+            } else if ((2*pos)+1 < tamaño) {
+                T elemMovedizo = elementos.get(pos);
+                siftDown(new int[]{pos,(2*pos)+1});
+                if (elementos.get(pos).equals(elemMovedizo)) {
+                    //no me movi
+                    pos = tamaño;
+                }else{
+                    //me fui al hijo
+                    pos = (2*pos)+1;
+                }
+            } else {
+                pos = tamaño;
+            }
+        }
+    }
+
+    return hashirama;
+}
+
+public void Anhadir(T nuevo){
+    elementos.add(nuevo);
+    tamaño++;
+    int pos = tamaño - 1;
+    while ( pos > 0 || ((pos-1)/2) >= 0 ) {
+        T viejo = elementos.get(pos);
+        siftUp(new int[]{(pos-1)/2,pos});
+        if (elementos.get(pos).equals(viejo)) {
+            pos = 0;
+        }
+        pos = (pos-1)/2;
+    }
+}
+
+
 public void heapify(int pos){
     if(tamaño > 2 && pos>1){
         if(elementos.get((pos-1)/2) == elementos.get((pos-2)/2)){
@@ -29,6 +100,16 @@ public void heapify(int pos){
         }
     }else if(tamaño == 2){
         siftDown(new int[]{0,1});
+    }
+}
+
+public void siftUp(int[] elems){
+    T vader = elementos.get(elems[0]);
+    T luke = elementos.get(elems[1]);
+
+    if (comparador.compare(vader, luke) < 0) {
+        elementos.set(elems[0], luke);
+        elementos.set(elems[1], vader);
     }
 }
 
@@ -58,6 +139,10 @@ public static void main(String[] args) {
     ciudad1.incr_ganancia(10); //10 super
     ciudad2.incr_perdida(400); //-400 super
     Heap<Ciudad> heap = new Heap<Ciudad>(new Ciudad[]{ciudad2,ciudad1,ciudad0}, SuperavitComparator);
+    Ciudad gotica = new Ciudad(3);
+    gotica.incr_perdida(-3000);
+    // System.out.println(heap.sacarMaximo());
+    heap.Anhadir(gotica);
 }
 
 }
